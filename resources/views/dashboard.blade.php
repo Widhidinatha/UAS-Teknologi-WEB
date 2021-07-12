6a9457810e57189a7e174ff4be4744ed00fc7b70
edinarendra <edinarendra@github.com>
2021-07-12 18:05:03 +0800

    tugasuas

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="h-1 w-full gradient my-0 py-0 rounded-t"></div>
+    <img center src="kisspng-clothing-accessories-fashion-vector-graphics-shoe-5c02ad9bcc6d50.6093499515436793878373-removebg-preview.png">
+    </div>
+</x-app-layout>
